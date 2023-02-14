b59c3f0cee44ce51f199ce8842b4fb14ea8239a7
miss-f-g <fatemeghafari98@gmail.com>
2023-02-14 17:58:20 +0330

    ep85 - dynamic route

---
+++ pages/menu/[id].js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Details = () => {
+  return <div>Details</div>;
+};
+
+export default Details;
+
+export async function getStaticPaths() {
+  const res = await fetch("http://localhost:4000/data");
+  const json = await res.json();
+  const data = json.slice(0, 10);
+
+  const paths = data.map((food) => ({ params: { id: food.id.toString() } }));
+  return { paths, fallback: true };
+}
