82fabf5ee13dae758c9a43f59d5c29d09eaaa028
miss-f-g <fatemeghafari98@gmail.com>
2023-02-14 12:14:04 +0330

    ep83 - show data in menuPage

--- components/templates/MenuPage.js
+++ components/templates/MenuPage.js
@@ -1,11 +1,14 @@
 import styles from "./MenuPage.module.css";
 
-
-const MenuPage = ({data}) => {
-    console.log(data);
+const MenuPage = ({ data }) => {
   return (
-    <div>MenuPage</div>
-  )
-}
+    <div className={styles.container}>
+      <h2>Menu</h2>
+      <div className={styles.subContainer}>
+        {data.map(food=> <p key={food.id} >{food.name}</p>)}
+      </div>
+    </div>
+  );
+};
 
-export default MenuPage
+export default MenuPage;
