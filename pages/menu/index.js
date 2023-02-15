a421d06f9c5e11d4292434bd45a42fbad171f1c9
miss-f-g <fatemeghafari98@gmail.com>
2023-02-15 14:07:41 +0330

    ep94

--- pages/menu/index.js
+++ pages/menu/index.js
@@ -13,7 +13,6 @@ export default Menu;
 export async function getStaticProps() {
   const res = await fetch("http://localhost:4000/data");
   const data = await res.json();
-  console.log(data);
 
   return {
     props: { data },
